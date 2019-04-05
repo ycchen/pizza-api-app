06bb3b708a5056bd118f9928ea355c6299766106
Yungchih Chen <yungchih_chen@yahoo.com>
2019-04-05 17:44:34 -0400

    Add JWT and authroize_request method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
+  
+  def not_found
+    render json: { error: 'not_found' }
+  end
+
+  def authorize_request
+    header = request.headers['Authorization']
+    header = header.split(' ').last if header
+
+    begin
+      @decoded = JsonWebToKen.decode(header)
+      @current_user = User.find(@decoded[:user_id])
+    rescue ActiveRecord::RecordNotFound => begin
+      render json: { error: e.message }, status: :unauthorized
+    rescue JWT::DecordError => e
+      render json: { error: e.message }, status: :unauthorized
+    end
+  end
 end
